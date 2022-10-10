a1ceda9f423832bcc89eaeb6c04576a943808574
joao vitor correa <joaov1997@hotmail.com>
2022-10-10 09:05:31 -0300

    .

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,39 @@
+import userEvent from '@testing-library/user-event';
+import { Button } from '.';
+const { render, screen } = require('@testing-library/react');
+
+describe('<Button/>', () => {
+  it("should render the button with the text 'load more'", () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" onClick={fn} />);
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" onClick={fn} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    userEvent.click(button);
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be disabled when disabled is true', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" onClick={fn} disabled={true} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
+
+  it('should be enabled when disabled is false', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeEnabled();
+  });
+});
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import P from 'prop-types';
+
+import './styles.css';
+
+export class Button extends Component {
+  render() {
+    const { text, onClick, disabled } = this.props;
+
+    return (
+      <button className="button" onClick={onClick} disabled={disabled}>
+        {text}
+      </button>
+    );
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+Button.propTypes = {
+  text: P.string.isRequired,
+  onClick: P.func.isRequired,
+  disabled: P.bool,
+};
